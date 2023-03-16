e84150c67c79cd307b5a4bcd620263d63578463a
dizcza <dizcza@gmail.com>
2023-03-16 15:09:59 +0200

    removed unused fix for other mic

--- lib/audio_input/src/I2SMEMSSampler.h
+++ lib/audio_input/src/I2SMEMSSampler.h
@@ -6,7 +6,6 @@ class I2SMEMSSampler : public I2SSampler
 {
 private:
     i2s_pin_config_t m_i2sPins;
-    bool m_fixSPH0645;
 
 protected:
     void configureI2S();
@@ -15,7 +14,6 @@ public:
     I2SMEMSSampler(
         i2s_port_t i2s_port,
         i2s_pin_config_t &i2s_pins,
-        i2s_config_t i2s_config,
-        bool fixSPH0645 = false);
+        i2s_config_t i2s_config);
     virtual int read(int16_t *samples, int count);
 };
